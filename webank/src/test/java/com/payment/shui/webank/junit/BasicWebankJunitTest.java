cff45ea73e90a117f97dec8219d31a53570a6775
shuijiayou <602570224@qq.com>
2020-08-25 14:11:42 +0800

    解决webank模块项目无法启动问题，由于pom依赖冲突导致。

---
+++ webank/src/test/java/com/payment/shui/webank/junit/BasicWebankJunitTest.java
@@ -0,0 +1,15 @@
+package com.payment.shui.webank.junit;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author code
+ * @Title: AppTest
+ * @Copyright: Copyright (c) 2020
+ * @Description: <br>
+ * @Company: zyxf
+ * @Created on 2020/8/25 14:05
+ */
+@SpringBootTest
+public class BasicWebankJunitTest {
+}
